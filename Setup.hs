c01437ba1c34ebd5ce3ffa2c3685f8ce02a2ef78
Daniel Taskoff <daniel.taskoff@gmail.com>
2020-01-24 11:33:19 +0200

    Bundle a minimal subset of hwloc files with sdist

--- Setup.hs
+++ Setup.hs
@@ -1,4 +1,5 @@
 import Control.Monad (unless)
+import Data.List (intercalate)
 import Distribution.PackageDescription (emptyHookedBuildInfo)
 import Distribution.Simple (defaultMainWithHooks, simpleUserHooks, preBuild)
 import System.Directory (doesFileExist)
@@ -9,14 +10,30 @@ main :: IO ()
 main = defaultMainWithHooks simpleUserHooks
   { preBuild = \_ _ -> do
       let process command = putStrLn =<< readCreateProcess (shell command) { cwd = Just "./hwloc" } ""
+          configuration =
+            [ "--enable-static"
+            , "--disable-shared"
+            , "--disable-picky"
+            , "--disable-cairo"
+            , "--disable-cpuid"
+            , "--disable-libxml2"
+            , "--disable-io"
+            , "--disable-pci"
+            , "--disable-opencl"
+            , "--disable-cuda"
+            , "--disable-nvml"
+            , "--disable-gl"
+            , "--disable-libudev"
+            , "--disable-netloc"
+            ]
 
       autogend <- doesFileExist "hwloc/configure"
       configured <- (autogend &&) <$> doesFileExist "hwloc/Makefile"
       made <- (configured &&) <$> doesFileExist "hwloc/hwloc/.libs/libhwloc.a"
 
       unless autogend $ process "./autogen.sh"
-      unless configured $ process "./configure --enable-static --disable-shared"
-      unless made $ process "make LDFLAGS=-all-static"
+      unless configured $ process $ intercalate " " $ "./configure" : configuration
+      unless made $ process "make -C hwloc LDFLAGS=-all-static"
 
       pure emptyHookedBuildInfo
   }
