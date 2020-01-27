6e6db7490c66aab0cb714c95f61d7df82cf0c1c7
Daniel Taskoff <daniel.taskoff@gmail.com>
2020-01-27 14:37:27 +0200

    Merge the static libraries post build

--- Setup.hs
+++ Setup.hs
@@ -1,15 +1,13 @@
+{-# options_ghc -Wall -Werror -fwarn-incomplete-uni-patterns -fwarn-incomplete-record-updates #-}
 {-# Language NamedFieldPuns #-}
+
 import Control.Monad (unless)
-import Data.List (intercalate)
 import Distribution.PackageDescription (emptyHookedBuildInfo)
-import Distribution.Simple (compilerInfo, defaultMainWithHooks, preBuild, postReg, simpleUserHooks)
-import Distribution.Simple.InstallDirs
-  ( InstallDirs(..), combinePathTemplate, fromPathTemplate, toPathTemplate
-  , initialPathTemplateEnv, substituteInstallDirTemplates
-  )
-import Distribution.Simple.Setup (BuildFlags(buildVerbosity), RegisterFlags(regVerbosity), fromFlagOrDefault)
+import Distribution.Simple (defaultMainWithHooks, preBuild, postBuild, simpleUserHooks)
+import Distribution.Simple.InstallDirs (combinePathTemplate, fromPathTemplate, toPathTemplate)
+import Distribution.Simple.Setup (BuildFlags(..), fromFlagOrDefault)
 import Distribution.Simple.Utils (findFileWithExtension, maybeExit, rawSystemExit, rawSystemIOWithEnv)
-import Distribution.Types.LocalBuildInfo (LocalBuildInfo(..), localCompatPackageKey, localPackage, localUnitId)
+import Distribution.Types.LocalBuildInfo (LocalBuildInfo(..), localCompatPackageKey)
 import Distribution.Verbosity (normal)
 
 
@@ -18,9 +16,9 @@ main = defaultMainWithHooks simpleUserHooks
   { preBuild = \_ flags -> do
       let verbosity = fromFlagOrDefault normal $ buildVerbosity flags
           rawSystemIO command args =
-            maybeExit $ rawSystemIOWithEnv verbosity command args (Just "hwloc") Nothing Nothing Nothing Nothing
-          doesFileExist file = maybe False (const True) <$>
-            findFileWithExtension [""] ["hwloc"] file
+            maybeExit $
+              rawSystemIOWithEnv verbosity command args (Just "hwloc") Nothing Nothing Nothing Nothing
+          doesFileExist file = maybe False (const True) <$> findFileWithExtension [""] ["hwloc"] file
 
       autogend <- doesFileExist "configure"
       configured <- (autogend &&) <$> doesFileExist "Makefile"
@@ -48,13 +46,9 @@ main = defaultMainWithHooks simpleUserHooks
 
       pure emptyHookedBuildInfo
 
-  , postReg = \_ flags _ lbi@LocalBuildInfo { compiler, hostPlatform, installDirTemplates } -> do
-      let pathTemplateEnv = initialPathTemplateEnv (localPackage lbi) (localUnitId lbi) (compilerInfo compiler) hostPlatform
-          InstallDirs { libdir, libsubdir } = substituteInstallDirTemplates pathTemplateEnv installDirTemplates
-          libDir = combinePathTemplate libdir libsubdir
-          libName = toPathTemplate $ "libHS" <> localCompatPackageKey lbi <> ".a"
-          libPath = fromPathTemplate $ combinePathTemplate libDir libName
-          verbosity = fromFlagOrDefault normal $ regVerbosity flags
-
-      rawSystemExit verbosity "libtool" ["-static", "-o", libPath, libPath, "hwloc/hwloc/.libs/libhwloc.a"]
+  , postBuild = \_ BuildFlags { buildVerbosity } _ lbi@LocalBuildInfo { buildDir } -> do
+      let libName = toPathTemplate $ "libHS" <> localCompatPackageKey lbi <> ".a"
+          libPath = fromPathTemplate $ combinePathTemplate (toPathTemplate buildDir) libName
+          verbosity = fromFlagOrDefault normal buildVerbosity
+       in rawSystemExit verbosity "libtool" ["-static", "-o", libPath, libPath, "hwloc/hwloc/.libs/libhwloc.a"]
   }
