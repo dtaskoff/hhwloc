e93f786a5d7d2ffdb7540f30028b402749baf3e4
Daniel Taskoff <daniel.taskoff@gmail.com>
2020-02-03 19:54:08 +0200

    Use ghc-options -optl instead of linking the dynamic library by hand

--- Setup.hs
+++ Setup.hs
@@ -4,19 +4,16 @@
 
 import Control.Monad (unless)
 import Data.List (isSuffixOf)
+import Distribution.Compiler (CompilerFlavor(GHC))
 import Distribution.ModuleName (toFilePath)
-import Distribution.PackageDescription (PackageDescription(..), emptyHookedBuildInfo, explicitLibModules)
-import Distribution.Pretty (prettyShow)
+import Distribution.PackageDescription (PackageDescription(..), explicitLibModules)
 import Distribution.Simple (defaultMainWithHooks, preBuild, postBuild, simpleUserHooks)
-import Distribution.Simple.Compiler (compilerVersion)
-import Distribution.Simple.PackageIndex (allPackages)
 import Distribution.Simple.Setup (BuildFlags(..), fromFlagOrDefault)
 import Distribution.Simple.Utils (rawSystemExit)
-import Distribution.Types.InstalledPackageInfo (InstalledPackageInfo(..))
 import Distribution.Types.LocalBuildInfo (LocalBuildInfo(..), localCompatPackageKey)
+import Distribution.Types.BuildInfo (BuildInfo(..), emptyBuildInfo)
 import Distribution.Verbosity (normal)
 import System.Directory (doesDirectoryExist, doesFileExist, listDirectory, withCurrentDirectory)
-import System.Info (os)
 
 
 main :: IO ()
@@ -48,7 +45,9 @@ main = defaultMainWithHooks simpleUserHooks
           ]
         unless made $ rawSystemExit verbosity "make" ["-C", "hwloc"]
 
-        pure emptyHookedBuildInfo
+        let buildInfo = emptyBuildInfo { sharedOptions = [(GHC, ["-optlhwloc/hwloc/.libs/libhwloc.a"])] }
+
+        pure (Just buildInfo, [])
 
   , postBuild = \_ BuildFlags { buildVerbosity } PackageDescription { library } localBuildInfo ->
       case explicitLibModules <$> library of
@@ -57,51 +56,17 @@ main = defaultMainWithHooks simpleUserHooks
           hwlocLibs <- listDirectory "hwloc/hwloc/.libs"
 
           let verbosity = fromFlagOrDefault normal buildVerbosity
-              LocalBuildInfo { buildDir, compiler, installedPkgs } = localBuildInfo
+              LocalBuildInfo { buildDir } = localBuildInfo
 
               hwlocObjects = [ "hwloc/hwloc/.libs" </> file | file <- hwlocLibs, ".o" `isSuffixOf` file ]
               hhwlocModulesPaths = map ((buildDir </>) . toFilePath) modules
               hhwlocObjects = map (<.> "o") hhwlocModulesPaths
-              hhwlocDynObjects = map (<.> "dyn_o") hhwlocModulesPaths
 
               libHShwloc = "libHS" <> localCompatPackageKey localBuildInfo
               libPath = buildDir </> libHShwloc <.> "a"
-              dynLibHShwloc = libHShwloc <> "-ghc" <> prettyShow (compilerVersion compiler)
-
-              commonOptions = [ "-fno-stack-protector", "-DTABLES_NEXT_TO_CODE" ]
-              rpaths = map ("-Wl,-rpath," <>) libDirs
-              libraries = concat
-                [ map ("-L" <>) libDirs
-                , map ("-l" <>) $ concatMap hsLibraries pkgs ++ concatMap extraLibraries pkgs
-                ]
-              libDirs = concatMap libraryDirs pkgs
-              pkgs = allPackages installedPkgs
 
           rawSystemExit verbosity "ar" $ ["-r", libPath] ++ hwlocObjects ++ hhwlocObjects
           -- ^ create a new static library with all of libhwloc's and hhwloc's objects
-
-          case os of
-            "darwin" ->
-              rawSystemExit verbosity "gcc" $
-                concat
-                  [ commonOptions, rpaths
-                  , [ "-dynamiclib", "-o", buildDir </> dynLibHShwloc <.> "dylib"
-                    , "-undefined", "dynamic_lookup", "-single_module"
-                    , "-install_name", "@rpath" </> dynLibHShwloc <.> "dylib"
-                    ]
-                  , hwlocObjects ++ hhwlocDynObjects, libraries, ["-Wl,-dead_strip_dylibs"]
-                  ]
-            "linux" ->
-              rawSystemExit verbosity "gcc" $
-                concat
-                  [ commonOptions, ["-fuse-ld=gold", "-Wl,--no-as-needed"], rpaths
-                  , [ "-o", buildDir </> dynLibHShwloc <.> "so", "-shared", "-Wl,-Bsymbolic"
-                    , "-Wl,-h," <> dynLibHShwloc <.> "so"
-                    ]
-                  , hwlocObjects ++ hhwlocDynObjects, libraries
-                  ]
-            _ -> pure ()
-          -- ^ create a new dynamic library with all of libhwloc's and hhwloc's objects
   }
 
 (</>), (<.>) :: FilePath -> FilePath -> FilePath
